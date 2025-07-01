97144e3e6aaf32293a9b588f09973989dd5de1ee
Marc Barbar <92314313+carcmarc@users.noreply.github.com>
2025-07-01 16:55:41 -0400

    fix timeseries

--- src/carbonarc/base/utils.py
+++ src/carbonarc/base/utils.py
@@ -5,17 +5,40 @@ from typing import Union
 def timeseries_response_to_pandas(response: Union[dict, pd.DataFrame]) -> pd.DataFrame:
     """
     Convert a timeseries response to a pandas DataFrame.
-    :param response: The response object from the API.
-    :return: A pandas DataFrame containing the timeseries data.
+
+    Args:
+        response: The response object from the API.
+
+    Returns:
+        A pandas DataFrame containing the timeseries data.
     """
+    
     if isinstance(response, pd.DataFrame):
         response["date"] = pd.to_datetime(response["date"])
         return response
+    
     elif isinstance(response, dict):
-        current_page_data = response["data"]
-        df = pd.DataFrame(current_page_data)
-        df["date"] = pd.to_datetime(df["date"])
+        response_data = response.get("data", [])
+        if not response_data:
+            raise ValueError("Response data is empty")
+        
+        series = []
+        for item in response_data:
+            df = pd.DataFrame(item["series"])
+            df["date"] = pd.to_datetime(df["date"])
+            df["entity_representation"] = item["entity_representation"]
+            df["entity_id"] = item["entity_id"]
+            df["entity_name"] = item["entity_name"]
+            
+            df.rename(columns={"value": item["insight"]}, inplace=True)
+            
+            df = df[["entity_representation", "entity_id", "entity_name", "date", item["insight"]]]
+            series.append(df)
+        
+        df = pd.concat(series)
+        
         return df
+        
     else:
         raise ValueError("Response must be a dictionary or a pandas DataFrame")
 
@@ -23,8 +46,12 @@ def timeseries_response_to_pandas(response: Union[dict, pd.DataFrame]) -> pd.Dat
 def is_valid_date(date_string: str) -> bool:
     """
     Checks if a string is a valid date in YYYY-MM-DD or YYYY-MM-DDTHH:MM:SS format.
-    :param date_string: The date string to check.
-    :return: True if the date string is valid, False otherwise.
+    
+    Args:
+        date_string: The date string to check.
+
+    Returns:
+        True if the date string is valid, False otherwise.
     """
     formats = ['%Y-%m-%d', '%Y-%m-%dT%H:%M:%S']
     for fmt in formats:
